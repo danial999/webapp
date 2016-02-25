049bb8274fd3fd27b8d4351d67dcb5cdcfd35056
danial999 <danial.ibraheem@gmail.com>
2016-02-25 04:39:07 +0200

    fixed search behavior

--- js/main.js
+++ js/main.js
@@ -1,96 +1,108 @@
-function Initiate() {
+flag =1;
 
+function Initiate() {
   debugger;
+
+   
  var count_rep = -1;
  var count_fol = -1;
-  var hash = window.location.hash.substring(1);
-  if((hash == "quick-reports")||(hash=='')){
+ var hash = window.location.hash.substring(1);
+ if((hash == "quick-reports")||(hash=='')){
   var elem = document.getElementById("wrap-input");
   var settings = document.getElementById("settings");
   var frame = document.querySelector('#frame2');
   var list = document.querySelector('#links-list');
   var extend = document.querySelector('.expand2');
   var cancel = 'cancel';
-  } else {
+} else {
   var elem = document.getElementById("wrap-input1");
   var settings = document.getElementById("settings1");
   var frame = document.querySelector('#frame3');
   var list = document.querySelector('#links-list2');
   var extend = document.querySelector('.expand3');
-   var cancel = 'cancel1';
-  }
-  OnRes();
-  removeOptions(list);
-  for (var i = 0; i < localStorage.length; i=i+2){
-  
-    
-    var name = localStorage.getItem(localStorage.key(i));
-    var str1 = localStorage.key(i).split("_", 1);
-    var url = localStorage.getItem(localStorage.key(i+1));
-    var str2 = localStorage.key(i+1).split("_", 2).pop();
-    var res = localStorage.key(i).split("_").pop();
-    var input_name =  document.getElementById(str1);
-    var input_url =  document.getElementById(str2);
-    if((res == "report")&&((hash == "quick-reports")||(hash==''))){
-   
-   
+  var cancel = 'cancel1';
+}
+
+
+OnRes();
+removeOptions(list);
+for (var i = 0; i < localStorage.length; i=i+2){
+  var name = localStorage.getItem(localStorage.key(i));
+  var str1 = localStorage.key(i).split("_", 1);
+  var url = localStorage.getItem(localStorage.key(i+1));
+  var str2 = localStorage.key(i+1).split("_", 2).pop();
+  var res = localStorage.key(i).split("_").pop();
+  var input_name =  document.getElementById(str1);
+  var input_url =  document.getElementById(str2);
+  if((res == "report")&&((hash == "quick-reports")||(hash==''))){
     var opt = document.createElement('option');
     count_rep++;
-       
-        input_name.value = name;
-        input_url.value = url;
-         opt.value = url;
-         opt.innerHTML = name;
-         list.appendChild(opt);
-         if(count_rep==0){
-           extend.href = url;
-         frame.src = url;
-       }
-     }
-   else if ((res == "folder")&&(hash == "my-team-folders")){
-      count_fol++;
-       var opt = document.createElement('option');
 
-
-        input_name.value = name;
-        input_url.value = url;
-         opt.value = url;
-         opt.innerHTML = name;
-         list.appendChild(opt);
-         if(count_fol==0){
-           extend.href = url;
-         frame.src = url;
-       }
-     }
+    input_name.value = name;
+    input_url.value = url;
+    opt.value = url;
+    opt.innerHTML = name;
+    list.appendChild(opt);
+    if(count_rep==0){
+     extend.href = url;
+     frame.src = url;
    }
+ }
+ else if ((res == "folder")&&(hash == "my-team-folders")){
+  count_fol++;
+  var opt = document.createElement('option');
+  input_name.value = name;
+  input_url.value = url;
+  opt.value = url;
+  opt.innerHTML = name;
+  list.appendChild(opt);
+  if(count_fol==0){
+   extend.href = url;
+   frame.src = url;
+ }
+}
+}
 
+if(flag !=1){
+OnChange(flag);
+if(hash == "my-team-folders"){
+ var list = document.querySelector('#links-list2');
+}
+else{
+  var list = document.querySelector('#links-list');
+}
+     str1 =str1 + "";
+     var num = str1.substring(4, 5);
+    list.selectedIndex = (num-1);
+    flag=1;
+}
 
+if(frame.src!="about:blank"){
+  list.style.display = "block"
+  cancel_input(cancel);
 
-  if(frame.src!="about:blank"){
-    list.style.display = "block"
-    cancel_input(cancel);
-
-    extend.style.display = "block"
-    frame.style.display = "block"
-  }
-  else {
-    list.style.display = "none"
-    elem.style.display ="block";
-    settings.style.background="white";
-    frame.style.display = "none"
-  }
+  extend.style.display = "block"
+  frame.style.display = "block"
+}
+else {
+  list.style.display = "none"
+  elem.style.display ="block";
+  settings.style.background="white";
+  frame.style.display = "none"
+}
 }
 
 function OnChange(url) {
+  debugger;
   if (this.selectedIndex!==0) {
-      var hash = window.location.hash.substring(1);
-      var url = url;
-     if((hash == "quick-reports")||(hash=='')){
-    var frame = document.querySelector('#frame2');
-    var extend = document.querySelector('.expand2');
+    var hash = window.location.hash.substring(1);
+    var url = url;
+    if((hash == "quick-reports")||(hash=='')){
+      var frame = document.querySelector('#frame2');
+      var extend = document.querySelector('.expand2');
     }else {
       var frame = document.querySelector('#frame3');
-    var extend = document.querySelector('.expand3');
+      var extend = document.querySelector('.expand3');
     }
     extend.href = url;
     extend.style.display = "block"
@@ -123,7 +135,7 @@ function OnRes() {
     setStyle('.url' , {'margin-left':7+'%'});
   }
   else {
-     setStyle('.frame' , {'width': 97+ '%'});
+   setStyle('.frame' , {'width': 97+ '%'});
    setStyle('.page-body' , {'width':100+'%','margin-left':0+'px','margin-right':0+'px'});
    setStyle('.input' , {'width':98+'%','margin-left':1+'%','margin-right':1+'%'});
  }
@@ -138,11 +150,11 @@ function OnRes() {
 }
 function cancel_input(cancel) {
   if(cancel=="cancel"){
-  var elem = document.getElementById("wrap-input");
-  var settings = document.getElementById("settings");
+    var elem = document.getElementById("wrap-input");
+    var settings = document.getElementById("settings");
   }else{
-  var elem = document.getElementById("wrap-input1");
-  var settings = document.getElementById("settings1");
+    var elem = document.getElementById("wrap-input1");
+    var settings = document.getElementById("settings1");
   }
 
   settings.style.background="rgb(235, 235, 235)";
@@ -150,45 +162,45 @@ function cancel_input(cancel) {
 }
 
 function save_input(save) {
-  
-   if(save=="save"){
-    var start = "report"
+
+ if(save=="save"){
+  var start = "report"
   var elem = document.getElementById("wrap-input");
   var settings = document.getElementById("settings");
-  }else{
-    var start = "folder"
-    var elem = document.getElementById("wrap-input1");
+}else{
+  var start = "folder"
+  var elem = document.getElementById("wrap-input1");
   var settings = document.getElementById("settings1");
-  }
-  if(check == 2){
-    settings.style.background="rgb(235, 235, 235)";
-    elem.style.display ="none";
-    var len = localStorage.length;
-       if(save=="save"){
+}
+if(check == 2){
+  settings.style.background="rgb(235, 235, 235)";
+  elem.style.display ="none";
+  var len = localStorage.length;
+  if(save=="save"){
     var name = document.querySelectorAll('.link-name');
     var url = document.querySelectorAll('.link-url');
-    }else{
+  }else{
    var name = document.querySelectorAll('.link-name1');
-    var url = document.querySelectorAll('.link-url1');
+   var url = document.querySelectorAll('.link-url1');
+ }
+ var i;
+
+ if(name != null){
+  for (i = 0; i < name.length; ++i) {
+    len = localStorage.length;
+    if (name[i].value) {
+      localStorage.setItem(name[i].id+"_"+start, name[i].value);
     }
-    var i;
-   
-    if(name != null){
-      for (i = 0; i < name.length; ++i) {
-        len = localStorage.length;
-        if (name[i].value) {
-          localStorage.setItem(name[i].id+"_"+start, name[i].value);
-        }
-         if (url[i].value) {
-          localStorage.setItem(name[i].id+"_"+url[i].id+"_"+start, url[i].value);
-        }
+    if (url[i].value) {
+      localStorage.setItem(name[i].id+"_"+url[i].id+"_"+start, url[i].value);
+    }
        //  addEntry(name[i].value,url[i].value);
        // }
-      }
-   
-  Initiate();
-    } 
-  }
+     }
+
+     Initiate();
+   } 
+ }
   // if(check ==0 ){
   //   alert("Please check input");
   // }
@@ -205,7 +217,7 @@ function getSecondPart(str) {
 
 
 window.onhashchange = function(e){
-  debugger;
+   debugger;
   if(window.location.hash) {
     var oldURL = e.oldURL; 
     var newURL = e.newURL; 
@@ -269,7 +281,7 @@ window.onhashchange = function(e){
    li.style.color =  "black";
    li.style.background = "rgb(235, 235, 235)";
    setStyle(elem.id , {'display':'block', 'background':'rgb(235, 235, 235)'});
-       Initiate();
+   Initiate();
 
  }
  if(elem.id == "public-folders"){
@@ -284,7 +296,7 @@ window.onhashchange = function(e){
  li.style.background = "rgb(235, 235, 235)";
  setStyle(elem.id , {'display':'block', 'background':'rgb(235, 235, 235)'});
 
-    Initiate();
+ Initiate();
 }
 
 elem.style.height = "700px";
@@ -329,7 +341,7 @@ function name_url(name, url) {
   var name = document.getElementById(name);
   var url = document.getElementById(url);
   if(name.value=="") {
-      name.style.border = "1px solid red";
+    name.style.border = "1px solid red";
     check=0;
   }
   else if((url.value.slice(0,7) != "http://")&&(name.value.length>0)){
@@ -349,7 +361,7 @@ function url_check(name,url) {
   var name = document.getElementById(name);
   var url = document.getElementById(url);
   if(url.value=="") {
-      url.style.border = "1px solid red";
+    url.style.border = "1px solid red";
     check=0;
   }
   else if((url.value.slice(0,7) != "http://")&&(url.value.length)){
@@ -359,7 +371,7 @@ function url_check(name,url) {
   else 
   {
     check = 2;
-     url.style.border = "none";
+    url.style.border = "none";
     url.style.border = "none";
   }
 
@@ -371,6 +383,7 @@ function url_check(name,url) {
 
 
 window.onload = function(e) {
+     debugger;
   check=0;
 
   var  x=  window.location.pathname;
@@ -382,34 +395,73 @@ window.onload = function(e) {
 
 function removeOptions(selectbox)
 {
-    var i;
-    for(i=selectbox.options.length-1;i>=0;i--)
-    {
-        selectbox.remove(i);
-    }
+  debugger;
+  var i;
+  for(i=selectbox.options.length-1;i>=0;i--)
+  {
+    selectbox.remove(i);
+  }
 }
 
 function searchFn() {
-debugger;
-    for (var i = 0; i < localStorage.length; i=i+2){
-       var s = document.getElementById("search");
-       var name = localStorage.getItem(localStorage.key(i));
-        var str1 = localStorage.key(i).split("_", 1);
-        var url = localStorage.getItem(localStorage.key(i+1));
-        var str2 = localStorage.key(i+1).split("_", 2).pop();
-        var res = localStorage.key(i).split("_").pop();
-        if(name==s.value){
-          if(res == "report"){
-         
-         OnChange(url);
-           window.location.hash = "quick-reports";
-          }else{
-          OnChange(url);
-           window.location.hash ="my-team-folders";
-          }
-          
-        return;
+  
+   debugger;
+  for (var i = 0; i < localStorage.length; i=i+2){
+     var hash = window.location.hash.substring(1);
+   var s = document.getElementById("search");
+   var name = localStorage.getItem(localStorage.key(i));
+   var str1 = localStorage.key(i).split("_", 1);
+   var url = localStorage.getItem(localStorage.key(i+1));
+   
+   var str2 = localStorage.key(i+1).split("_", 2).pop();
+   var res = localStorage.key(i).split("_").pop();
+    if(name==s.value){
+   flag = url;
+    if(res == "report"){
+     
+      if(hash =="quick-reports" ){
+        OnChange(flag);
+        var list = document.querySelector('#links-list');
+         str1 =str1 + "";
+        var num = str1.substring(4, 5);
+        list.selectedIndex = (num-1);
+        flag=1;
+         }
+         else{
+      window.location.hash = "quick-reports";
       }
-    }
-    
+    return;
+   }else{ 
+      if(hash =="my-team-folders" ){
+        OnChange(flag);
+        var list = document.querySelector('#links-list2');
+         str1 =str1 + "";
+        var num = str1.substring(4, 5);
+        list.selectedIndex = (num-1);
+        flag=1;
+         } 
+         else{
+        window.location.hash ="my-team-folders";
+        }
+    return;
+   }
+
  }
+ 
+}
+
+  window.location.hash = "my-folders";
+
+  var search = document.querySelector('#search');
+  var frame = document.querySelector('#frame44');
+  var extend = document.querySelector('#expand44');
+  query = search.value;
+  str = "https://www.bing.com/search?q=";
+
+  str2 = str.concat(query);
+
+  frame.src = str2;
+  extend.href = str2;
+  return;
+
+}
